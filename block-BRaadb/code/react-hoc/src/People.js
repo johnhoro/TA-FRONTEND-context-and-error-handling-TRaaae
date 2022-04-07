9db6816fd7a0115391bf35f20e491a7080aeef9e
john horo <johnhoro125@gmail.com>
2022-04-07 20:55:47 +0530

    react-hoc

--- block-BRaadb/code/react-hoc/src/People.js
+++ block-BRaadb/code/react-hoc/src/People.js
@@ -1,40 +1,27 @@
 import React from "react";
-
 import people from "./data/got.json";
+import withSearch from "./WithSearch";
 
-class People extends React.Component {
-  state = {
-    searchTerm: "",
-  };
-  handleChange = ({ target: { value } }) => {
-    this.setState({ searchTerm: value });
-  };
-  render() {
-    let allPeople = people.reduce((acc, cv) => {
-      acc = acc.concat(cv.people);
-      return acc;
-    }, []);
+const People = (props) => {
+  let { searchTerm } = props;
+  let allPeople = people.reduce((acc, cv) => {
+    acc = acc.concat(cv.people);
+    return acc;
+  }, []);
 
-    let filteredPeople = allPeople.filter((person) =>
-      person.name.toLowerCase().includes(this.state.searchTerm)
-    );
-    return (
-      <>
-        <input
-          placeholder="Search"
-          className="search"
-          value={this.state.searchTerm}
-          onChange={this.handleChange}
-        />
-        <ul className="people">
-          {filteredPeople.map((sp) => (
-            <SinglePerson {...sp} />
-          ))}
-        </ul>
-      </>
-    );
-  }
-}
+  let filteredPeople = allPeople.filter((person) =>
+    person.name.toLowerCase().includes(searchTerm)
+  );
+  return (
+    <>
+      <ul className="people">
+        {filteredPeople.map((sp, i) => (
+          <SinglePerson key={i} {...sp} />
+        ))}
+      </ul>
+    </>
+  );
+};
 
 function SinglePerson(props) {
   return (
@@ -49,4 +36,4 @@ function SinglePerson(props) {
   );
 }
 
-export default People;
+export default withSearch(People);
